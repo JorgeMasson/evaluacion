7d4f41353098ab49fcf93933371f6930ad14f158
JorgeMasson <j_masson_reynoso@hotmail.com>
2021-08-25 19:13:30 -0700

    final

--- slnConfigEval/ConfigEval/Form1.cs
+++ slnConfigEval/ConfigEval/Form1.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
@@ -33,17 +34,10 @@ namespace ConfigEval
 
         private void btnSubmit_Click(object sender, EventArgs e)
         {
-            try
-            {
-                string sql = "insert into Employees values('" + txtEmployeeId.Text + "', '" + txtLastName.Text + "', '" + txtFirstName.Text + "', '" + txtDateOfBirth.Text + "')";
-                manejador.Ejecutar(sql);
-                MessageBox.Show("Empleado agregado con éxito");
-                BorrarCampos(this);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error" + ex);
-            }
+            string sql = "insert into Employees values('" + txtEmployeeId.Text + "', '" + txtLastName.Text + "', '" + txtFirstName.Text + "', '" + txtDateOfBirth.Text + "')";
+            manejador.Ejecutar(sql);
+            MessageBox.Show("Empleado agregado con éxito");
+            BorrarCampos(this);
         }
 
         private void btnCreateXmlFile_Click(object sender, EventArgs e)
